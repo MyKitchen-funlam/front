18d9f615f560bbd38ad0d79d37d752ee30a3daf9
Jesus Restrepo <74331067+JesusRestrepo@users.noreply.github.com>
2022-11-03 13:01:59 +0000

    upgrade buscar recetas

--- src/pages/BuscarRecetas.js
+++ src/pages/BuscarRecetas.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import Box from '@mui/material/Box'
+import TextField from '@mui/material/TextField'
 
 import '../styles/buscarrecetas.css'
 
@@ -6,7 +8,139 @@ const BuscarRecetas = () => {
   return (
     <>
       <section className='background_buscar'>
-
+        <div className='search__recipes'>
+          <div className='ingredientes_seleccionados'>
+            <h2 className='titulo_recetas'>Ingredientes seleccionados:</h2>
+            <div className='seleccion'>
+              <Box
+                component="form"
+                sx={{
+                  '& .MuiTextField-root': { m: 1, width: 'fullwidth' },
+                }}
+                noValidate
+                autoComplete="off"
+              >
+                <TextField
+                  disabled
+                  id="outlined-disabled"
+                  defaultValue="seleccion"
+                />
+                <TextField
+                  disabled
+                  id="outlined-disabled"
+                  defaultValue="seleccion"
+                />
+                <TextField
+                  disabled
+                  id="outlined-disabled"
+                  defaultValue="seleccion"
+                />
+                <TextField
+                  disabled
+                  id="outlined-disabled"
+                  defaultValue="seleccion"
+                />
+                <TextField
+                  disabled
+                  id="outlined-disabled"
+                  defaultValue="seleccion"
+                />
+                <TextField
+                  disabled
+                  id="outlined-disabled"
+                  defaultValue="seleccion"
+                />
+                <TextField
+                  disabled
+                  id="outlined-disabled"
+                  defaultValue="seleccion"
+                />
+              </Box>
+            </div>
+          </div>
+          <div className='seleccion_ingredientes_y_muestra'>
+            <div className='buscar'>
+              <Box
+                component="form"
+                sx={{
+                  '& .MuiTextField-root': { m: 1, width: '90%' },
+                }}
+                noValidate
+                autoComplete="off"
+              >
+                <TextField
+                  sx={{
+                    input: {
+                      color: "black",
+                      background: "white"
+                    }
+                  }}
+                  fullWidth
+                  required
+                  label="Buscar"
+                  id="outlined-required"
+                />
+              </Box>
+            </div>
+            <Box
+                component="form"
+                sx={{
+                  '& .MuiTextField-root': { m: 1, width: 'maxwidth' },
+                }}
+                noValidate
+                autoComplete="off"
+              >
+                <TextField
+                  sx={{
+                    input: {
+                      color: "black",
+                      background: "white"
+                    }
+                  }}
+                  fullWidth
+                  required
+                  label="Buscar"
+                  id="outlined-required"
+                />
+                <TextField
+                  sx={{
+                    input: {
+                      color: "black",
+                      background: "white"
+                    }
+                  }}
+                  fullWidth
+                  required
+                  label="Buscar"
+                  id="outlined-required"
+                />
+                <TextField
+                  sx={{
+                    input: {
+                      color: "black",
+                      background: "white"
+                    }
+                  }}
+                  fullWidth
+                  required
+                  label="Buscar"
+                  id="outlined-required"
+                />
+                <TextField
+                  sx={{
+                    input: {
+                      color: "black",
+                      background: "white"
+                    }
+                  }}
+                  fullWidth
+                  required
+                  label="Buscar"
+                  id="outlined-required"
+                />
+              </Box>
+          </div>
+        </div>
       </section>
     </>
   )
